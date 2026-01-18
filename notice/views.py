d70b9cfa02bd8805c42279e9e27a4585d0f0dc0c
Faysal07 <faysalraktimurlmain@gmail.com>
2026-01-18 23:57:27 +0600

    Notice View update

--- notice/views.py
+++ notice/views.py
@@ -1,19 +1,22 @@
 from django.shortcuts import render
 from .models import Notice
 
+# Q Import Here
+from django.db.models import Q
+
 # Create your views here.
 def noticeLists(request):
     
     # Start With all Notice
     notices = Notice.objects.all()
     
-    # # Search Functionality
-    # search_quary = request.GET.get('search', '')
-    # if search_quary:
-    #     notices = notices.filter(
-    #         Q(noticeTitle__icontains = search_quary) |
-    #         Q(noticeCatagory__icontains = search_quary)
-    #     )
+    # Search Functionality
+    search_quary = request.GET.get('search', '')
+    if search_quary:
+        notices = notices.filter(
+            Q(noticeTitle__icontains=search_quary) |
+            Q(noticeCatagory__catagoryName__icontains=search_quary)
+        )
     
     # #Filter by Title
     # title_filter = request.GET.get('title', '')
@@ -21,5 +24,10 @@ def noticeLists(request):
     #     notices = notices.filter(noticeTitle = title_filter)
     
     
-    return render (request, 'notice.html', {"notices": notices})
+    # return render (request, 'notice.html', {"notices": notices})
+
+    return render(request, 'notice.html', {
+        "notices": notices,
+        "search_quary": search_quary
+    })
     
